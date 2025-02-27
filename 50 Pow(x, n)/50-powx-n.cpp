1482fddecc9241a51131fbebc8815f99213aaa3f
CodeSync <codesync@local.dev>
2025-02-27 16:46:54 +0000

    Add: 50. Pow(x, n)

---
+++ 50 Pow(x, n)/50-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1;
+        long long power=n;
+        if(power<0){
+            
+          x=1/x;
+          power=-power;
+        }
+        while(power > 0){
+            if(power%2==1){
+                ans*=x;
+            }
+            x*=x;
+            power/=2;
+        }
+        return ans;
+    }
+};
