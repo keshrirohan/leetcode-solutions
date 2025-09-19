202376971a4f89b7c06f8054e996dbc62ec8a0ec
CodeSync <codesync@local.dev>
2025-09-19 05:15:39 +0000

    Add: 1054. Complement of Base 10 Integer

---
+++ 1054 Complement of Base 10 Integer/1054-complement-of-base-10-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int notn = ~n, mask = 0;
+        while (n != 0) {
+            mask = mask << 1 | 1;
+            n = n >> 1;
+        }
+        int ans = notn & mask;
+        return ans;
+    }
+};
