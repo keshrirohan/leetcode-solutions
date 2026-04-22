f4d0b89a059bccdcb5c5527e9ef0934aba749862
CodeSync <codesync@local.dev>
2026-04-22 17:05:25 +0000

    Add: 66. Plus One

---
+++ 66 Plus One/66-plus-one.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int i=digits.size()-1;
+        while (i >= 0) {
+            if (digits[i] + 1 != 10) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+            i--;
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
