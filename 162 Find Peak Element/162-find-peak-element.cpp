f83111728fd2bf96d7f6d5c86341902b67ddadfe
CodeSync <codesync@local.dev>
2026-05-23 13:44:50 +0000

    Add: 162. Find Peak Element

---
+++ 162 Find Peak Element/162-find-peak-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int s=0,e=nums.size()-1;
+        int mid;
+        
+        int maxval=INT_MIN;
+        while(s<e){
+            mid=s+(e-s)/2;
+            if(nums[mid]<nums[mid+1]){
+                s=mid+1;
+            }
+            else{
+                e=mid;
+            }
+           
+        }
+        return e;
+            }
+};
